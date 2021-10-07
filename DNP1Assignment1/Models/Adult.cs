d88645ad9d1f47bb00a149959f660ecc8cf126e7
Lokkaze <74102422+Lokkaze@users.noreply.github.com>
2021-10-07 16:21:06 +0200

    init

---
+++ DNP1Assignment1/Models/Adult.cs
@@ -0,0 +1,5 @@
+namespace DNP1Assignment1.Models {
+public class Adult : Person {
+    public Job JobTitle { get; set; }
+}
+}
---
+++ DNP1Assignment1/Models/Job.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DNP1Assignment1.Models
+{
+    public class Job
+    {
+        public string JobTitle { get; set; }
+        public int Salary { get; set; }
+    }
+}
