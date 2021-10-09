da668a47fe1d43213c00be5f4e04578ad9e70119
Lokkaze <74102422+Lokkaze@users.noreply.github.com>
2021-10-09 19:38:56 +0200

    Authentication done

---
+++ DNP1Assignment1/Data/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using DNP1Assignment1.Models;
+
+namespace DNP1Assignment1.Data
+{
+    public interface IUserService
+    {
+        User ValidateUser(string userName, string Password);
+    }
+}
---
+++ DNP1Assignment1/Data/InMemoryUserService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DNP1Assignment1.Models;
+
+
+namespace DNP1Assignment1.Data
+{
+    public class InMemoryUserService : IUserService
+    {
+        private List<User> users;
+
+        public InMemoryUserService()
+        {
+            users = new[]
+            {
+                new User
+                {
+                    UserName = "lokkazy", Password = "oguricap", SecurityLevel = 1
+                },
+                new User
+                {
+                    UserName = "Inui", Password = "toko", SecurityLevel = 2
+                }
+            }.ToList();
+        }
+
+        public User ValidateUser(string userName, string password)
+        {
+            User first = users.FirstOrDefault(user => user.UserName.Equals(userName));
+            if (first == null)
+            {
+                throw new Exception("User not found");
+            }
+
+            if (!first.Password.Equals(password))
+            {
+                throw new Exception("Incorrect password");
+            }
+
+            return first;
+        }
+    }
+}
